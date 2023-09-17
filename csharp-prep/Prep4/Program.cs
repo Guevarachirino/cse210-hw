6932d2aeb39bf1105b053719dd6a62bc15a56075
Guevarachirino <chi23017@byui.edu>
2023-09-17 00:36:50 -0400

    updating

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+
+        
+        //creando lista 
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        //practicando Do while 
+        do 
+        {
+            
+            Console.Write("Enter number:");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            if(number != 0)
+            {
+                //agregando a la lista 
+                numbers.Add(number);
+
+            }
+        
+            int sum = 0;
+            foreach(int totalnumbers in numbers)
+            {
+                sum += totalnumbers;
+            }
+        
+                Console.WriteLine($"The sum is: {sum}");
+
+                float average = ((float)sum) / numbers.Count;
+                Console.WriteLine($"The average is: {average}");
+
+
+                int max = numbers[0];
+
+            foreach(int totalnumbers in numbers)
+            {
+                if (totalnumbers > max)
+                {
+                
+                max = totalnumbers;
+                }
+            }
+
+            Console.WriteLine($"The max is: {max}"); 
+        }
+    while (number != 0);
+        
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Console.WriteLine("Hello Prep5 World!");
+
+        string nombre = PromptUserName();
+        int numero = PromptUserNumber();
+        //int alcuadrado = SquareNumber(numero);
+        DisplayResult(nombre, numero);
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name:");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number:");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static double SquareNumber(double number)
+        {
+            //I have problem to convert double to int. 
+            double elevadoAlCuadrado = Math.Pow(number, 2); // Elevarlo a la potencia 2
+
+            double convirtiendo = Convert.ToInt32(elevadoAlCuadrado);
+            return convirtiendo;
+        
+            
+        }
+         static void DisplayResult(string name, int convirtiendo)
+        {
+            Console.WriteLine($"{name}, the square of your number is {convirtiendo}");
+        }
+
     }
 }
